e714a448d2cce55d96caaced99c43f79bc13d101
KhurramMunir95 <khurrammunir9522@gmail.com>
2024-05-31 18:17:45 +0500

    add product component + reducer added

--- src/features/product/productSlice.js
+++ src/features/product/productSlice.js
@@ -9,6 +9,9 @@ const productSlice = createSlice({
     name: 'products',
     initialState,
     reducers: {
+        addProduct: (state, {payload}) => {
+            state.products.push(payload);
+        },
         decreaseQuantity: (state, {payload}) => {
             const item = state.products.find((product) => product.id === payload.id);
             item.stock > 0 && item.stock--;
