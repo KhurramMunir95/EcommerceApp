5077efec0cce7b65a72443e2bb2b154fb110496a
KhurramMunir95 <khurrammunir9522@gmail.com>
2024-05-31 17:54:30 +0500

    components routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,21 @@
-import CartDetail from "./cart/CartDetail";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/assets/css/style.css';
-import Products from "./pages/Products";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from "./components/Header";
+import Products from "./pages/Products";
+import CartDetail from './pages/CartDetail';
 
 function App() {
 
   return (
     <>
-      <Header />
-      {/* <CartDetail /> */}
-      <Products />
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/products" element={<Products />} />
+          <Route path="/cart" element={<CartDetail />} />
+        </Routes>
+      </Router>
     </>
   )
 }
